e1f85d2c4d010c91fba94e7ae6633772186ead46
scroll550 <github.scroll550@silomails.com>
2026-04-21 06:30:37 -0700

    meta tag added to improve mobile rendering

--- scripts/buildWeb.js
+++ scripts/buildWeb.js
@@ -50,6 +50,7 @@ function writeRedirectPage(filePath, redirectTarget) {
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="refresh" content="0; url=${redirectTarget}" />
     <title>Redirecting…</title>
   </head>
